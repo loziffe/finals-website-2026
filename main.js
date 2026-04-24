6c101b3a0faa6b2f7ec1238a758123e2fb29b520
RebelScum <confuddled83@gmail.com>
2026-04-24 09:55:27 -0500

    added salato

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function disableScroll(){
-    const element = document.getElementById("disableScroll");
-    element.classList.add("paused")
+const fs = require('fs');
+const files = fs.readdirSync('./assets/buttons');
 
-}
+files.forEach(file => {
+  const img = document.createElement('img');
+    img.src = file;
+    img.alt = 'Hi';
+    document.footer.appendChild(img);
+
+});
 
 
-document.getElementById("disableScroll").onclick = disableScroll
+
+
+// file loop help https://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js
